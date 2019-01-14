f069f4e4d82c9daa30797c06c44a4e35899c31cc
Viachaslau Tyshkavets <pmstss@gmail.com>
2019-01-14 20:21:51 +0300

    admin quiz-list: refactoring to enable OnPush change detection

--- src/app/admin/components/quiz-list/quiz-list.component.ts
+++ src/app/admin/components/quiz-list/quiz-list.component.ts
@@ -1,25 +1,20 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
 import { Observable } from 'rxjs';
 import { QuizMeta, QuizService } from '../../../core';
 
 @Component({
     selector: 'app-quiz-list',
     templateUrl: './quiz-list.component.html',
-    styleUrls: ['./quiz-list.component.css']
+    styleUrls: ['./quiz-list.component.css'],
+    changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class QuizListComponent implements OnInit {
-    quizList: Observable<QuizMeta[]>;
-    adminMode = false;
+    quizList$: Observable<QuizMeta[]>;
 
-    constructor(private route: ActivatedRoute, private quizService: QuizService) {
-        this.route.data.subscribe((data) => {
-            this.adminMode = data.admin;
-        });
+    constructor(private quizService: QuizService) {
     }
 
     ngOnInit() {
-        console.log('### QuizListComponent onInit');
-        this.quizList = this.quizService.loadQuizList();
+        this.quizList$ = this.quizService.loadQuizList();
     }
 }
