902934729e4c2fb6a094cf7671f604dcc39e3e23
Viachaslau Tyshkavets <pmstss@gmail.com>
2019-01-14 20:20:50 +0300

    quiz-meta-editor: refactoring to enable OnPush change detection, UI improvements

--- src/app/admin/components/quiz-meta-editor/quiz-meta-editor.component.ts
+++ src/app/admin/components/quiz-meta-editor/quiz-meta-editor.component.ts
@@ -1,69 +1,70 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ChangeDetectionStrategy, ViewChild, ChangeDetectorRef } from '@angular/core';
+import { NgForm } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
-import { Subscription, from } from 'rxjs';
+import { from, Observable } from 'rxjs';
 import { flatMap } from 'rxjs/operators';
 import { NbToastrService } from '@nebular/theme';
-import { AutoUnsubscribe } from '../../../core';
 import { QuizAdminService } from '../../services/quiz-admin.service';
 import { QuizMetaAdmin } from '../../types/quiz-meta-admin';
 import { quillToolbarConfig } from '../quill-config';
-import { DialogService } from 'src/app/dialog/services/dialog.service';
+import { DialogService } from '../../../dialog';
 
 @Component({
     selector: 'app-quiz-meta-editor',
     templateUrl: './quiz-meta-editor.component.html',
-    styleUrls: ['./quiz-meta-editor.component.scss']
+    styleUrls: ['./quiz-meta-editor.component.scss'],
+    changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class QuizMetaEditorComponent implements OnInit {
-    quizMeta: QuizMetaAdmin;
-    @AutoUnsubscribe quizMetaSubscription: Subscription;
+    @ViewChild('form') public form: NgForm;
+    quizMeta$: Observable<QuizMetaAdmin>;
     quillToolbarConfig = quillToolbarConfig;
 
     constructor(private route: ActivatedRoute, private router: Router,
-                private toastrService: NbToastrService,
+                private changeDetectorRef: ChangeDetectorRef, private toastrService: NbToastrService,
                 private quizAdminService: QuizAdminService, private dialogService: DialogService) {
     }
 
     ngOnInit() {
-        this.quizMetaSubscription = this.route.params.pipe(
+        this.quizMeta$ = this.route.params.pipe(
             flatMap(params => (params.quizId === 'new' ? from([<QuizMetaAdmin><any>{
-                    randomizeItems: false,
-                    timeLimit: 30
-                }]) : this.quizAdminService.loadQuiz(params.quizId))
-            )
-        ).subscribe(quizMeta =>  this.quizMeta = quizMeta);
+                randomizeItems: false,
+                timeLimit: 30
+            }]) : this.quizAdminService.loadQuiz(params.quizId)))
+        );
     }
 
-    isNew() {
-        return this.quizMeta && !this.quizMeta.id;
+    isNew(quizMeta: QuizMetaAdmin) {
+        return quizMeta && !quizMeta.id;
     }
 
-    save() {
-        if (this.isNew()) {
-            this.quizAdminService.createQuiz(this.quizMeta).subscribe((quizMeta: QuizMetaAdmin) => {
-                this.toastrService.show(`Quiz "${quizMeta.shortName}", id: ${quizMeta.id}`, 'Quiz created!');
-                this.router.navigate([`/admin/quiz/${quizMeta.id}`]);
+    save(quizMeta: QuizMetaAdmin) {
+        if (this.isNew(quizMeta)) {
+            this.quizAdminService.createQuiz(quizMeta).subscribe((qm: QuizMetaAdmin) => {
+                this.toastrService.show(`Quiz "${qm.shortName}", id: ${qm.id}`, 'Quiz created!');
+                this.router.navigate([`/admin/quiz/${qm.id}`]);
             });
         } else {
-            this.quizAdminService.updateQuiz(this.quizMeta).subscribe((quizMeta: QuizMetaAdmin) => {
-                this.quizMeta = quizMeta;
-                this.toastrService.show(`Quiz "${quizMeta.shortName}", id: ${quizMeta.id}`, 'Quiz updated!');
+            this.quizAdminService.updateQuiz(quizMeta).subscribe((qm: QuizMetaAdmin) => {
+                this.toastrService.show(`Quiz "${qm.shortName}", id: ${qm.id}`, 'Quiz updated!');
+                this.form.form.markAsPristine();
+                this.changeDetectorRef.detectChanges();
             });
         }
     }
 
-    remove() {
+    remove(quizMeta: QuizMetaAdmin) {
         this.dialogService.confirm('Do you really want to remove whole quiz? Action can\'t be undone')
             .subscribe((comfirmed) => {
                 if (!comfirmed) {
                     return;
                 }
 
-                if (this.isNew()) {
+                if (this.isNew(quizMeta)) {
                     this.toastrService.show('Unsaved quiz removed', 'Quiz removed');
                     this.router.navigate(['/admin/quizes']);
                 } else {
-                    this.quizAdminService.deleteQuiz(this.quizMeta.id).subscribe((quizMeta: QuizMetaAdmin) => {
+                    this.quizAdminService.deleteQuiz(quizMeta.id).subscribe(() => {
                         this.toastrService.show(`Quiz "${quizMeta.shortName}", id: ${quizMeta.id}`, 'Quiz removed');
                         this.router.navigate(['/admin/quizes']);
                     });
