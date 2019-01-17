f8532bbaa57215be620058eef496d0e7906e8920
Viachaslau Tyshkavets <pmstss@gmail.com>
2019-01-17 22:42:46 +0300

    token-interceptor: bugfix

--- src/app/auth/interceptor/token-interceptor.ts
+++ src/app/auth/interceptor/token-interceptor.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@angular/core';
+import { Injectable, Inject } from '@angular/core';
 import {
     HttpRequest,
     HttpHandler,
@@ -6,22 +6,28 @@ import {
     HttpInterceptor
 } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { filter, switchMap, tap } from 'rxjs/operators';
+import { switchMap } from 'rxjs/operators';
 import { Store } from '@ngrx/store';
+import { BASE_URL_TOKEN } from '../../consts';
 import { AppState, selectToken } from '../../store';
 
 @Injectable()
 export class TokenInterceptor implements HttpInterceptor {
-    constructor(private appStore: Store<AppState>) {}
+    constructor(private appStore: Store<AppState>, @Inject(BASE_URL_TOKEN) private baseUrl) {}
 
     intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
         return this.appStore.select(selectToken).pipe(
-            filter(token => !!token),
-            switchMap(token => next.handle(request.clone({
-                setHeaders: {
-                    Authorization: `Bearer ${token}`
+            switchMap((token) => {
+                if (!token || !request.url.startsWith(this.baseUrl)) {
+                    return next.handle(request);
                 }
-            })))
+
+                return next.handle(request.clone({
+                    setHeaders: {
+                        Authorization: `Bearer ${token}`
+                    }
+                }));
+            })
         );
     }
 }
