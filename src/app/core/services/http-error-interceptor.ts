e5b2e066b1882bd741c9fae08857ad6d2c792ac6
Viachaslau Tyshkavets <pmstss@gmail.com>
2019-01-19 02:02:58 +0300

    clear token and redirect to login on 401

--- src/app/core/services/http-error-interceptor.ts
+++ src/app/core/services/http-error-interceptor.ts
@@ -9,15 +9,18 @@ import {
 import { Observable, throwError } from 'rxjs';
 import { retry, catchError } from 'rxjs/operators';
 import { MessageService } from './message.service';
+import { Router } from '@angular/router';
+import { NbTokenService } from '@nebular/auth';
 
 @Injectable({ providedIn: 'root' })
 export class HttpErrorInterceptor implements HttpInterceptor {
-    constructor(private messageService: MessageService) { }
+    constructor(private messageService: MessageService, private router: Router,
+                private tokenService: NbTokenService) { }
 
     intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
         return next.handle(request)
             .pipe(
-                retry(1), // TODO ### retryWhen
+                // retry(1), // TODO ### retryWhen
                 catchError((error: HttpErrorResponse) => {
                     if (error.error instanceof ErrorEvent) {
                         this.messageService.publishError({
@@ -31,6 +34,12 @@ export class HttpErrorInterceptor implements HttpInterceptor {
                         message: error.status === 0 ? 'Server is unavailable' :
                             error.error && error.error.message || error.message
                     });
+
+                    // TODO ### better indicator of token error?
+                    if (error.status === 401) {
+                        this.tokenService.clear();
+                        this.router.navigateByUrl('/auth/login');
+                    }
 
                     return throwError({
                         ...error,
