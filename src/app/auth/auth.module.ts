631301bfdc1ed572a5f179971cf7fc878e5d401a
Viachaslau Tyshkavets <pmstss@gmail.com>
2019-01-18 14:30:29 +0300

    configuring and using existing NbAuthJWTInterceptor instead of custom one (TokenInterceptor)

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
-import { NbAuthModule } from '@nebular/auth';
+import { NbAuthModule, NbAuthJWTInterceptor, NB_AUTH_TOKEN_INTERCEPTOR_FILTER } from '@nebular/auth';
 import { InverseAuthGuard } from './guards/inverse-auth.guard';
 import { AuthGuard } from './guards/auth.guard';
 import { AdminAuthGuard } from './guards/admin-auth.guard';
 import { nbAuthOptions } from './config/nb-auth-options';
-import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import { HTTP_INTERCEPTORS, HttpRequest } from '@angular/common/http';
 import { TokenInterceptor } from './interceptor/token-interceptor';
+import { BASE_URL } from '../consts';
 
 @NgModule({
     declarations: [],
@@ -14,8 +15,12 @@ import { TokenInterceptor } from './interceptor/token-interceptor';
     ],
     providers: [AuthGuard, InverseAuthGuard, AdminAuthGuard,
         {
+            provide: NB_AUTH_TOKEN_INTERCEPTOR_FILTER,
+            useValue: (req: HttpRequest<any>) => !req.url.startsWith(BASE_URL)
+        },
+        {
             provide: HTTP_INTERCEPTORS,
-            useClass: TokenInterceptor,
+            useClass: NbAuthJWTInterceptor,
             multi: true
         }
     ],
