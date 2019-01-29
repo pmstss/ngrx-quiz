5d72bcdfc9006371d3b84f308ea16c2cba17e00f
Viachaslau Tyshkavets <pmstss@gmail.com>
2019-01-30 02:01:10 +0300

    comments.component: observable logic simplification (moving answered check into showComments())

--- src/app/comments/components/comments/comments.component.ts
+++ src/app/comments/components/comments/comments.component.ts
@@ -1,12 +1,12 @@
-import { Component, Input, EventEmitter, Output, ChangeDetectionStrategy, OnInit } from '@angular/core';
+import { Component, Input, ChangeDetectionStrategy, OnInit } from '@angular/core';
 import { trigger, useAnimation, transition } from '@angular/animations';
-import { Observable, BehaviorSubject, of, Subscription } from 'rxjs';
+import { Observable, BehaviorSubject, of, Subscription, combineLatest } from 'rxjs';
 import { Store } from '@ngrx/store';
 import { Comment } from 'ngrx-quiz-common';
 import { fadeIn } from 'ng-animate';
 import { AppState, selectItemComments, selectActiveItemAnswered,
     ActionLoadItemComments, ActionPostItemComment, selectQuizStep } from '../../../store';
-import { switchMap, first, filter } from 'rxjs/operators';
+import { filter, map, take } from 'rxjs/operators';
 import { DialogService } from 'src/app/dialog';
 
 @Component({
@@ -23,8 +23,6 @@ export class CommentsComponent implements OnInit {
     @Input() numberOfComments: number = 0;
 
     private stepSubscription: Subscription;
-    private itemAnswered$: Observable<boolean>;
-    private itemComments$: Observable<Comment[]>;
     private commentsExpanded = new BehaviorSubject<boolean>(false);
 
     commentsExpanded$: Observable<boolean> = this.commentsExpanded.asObservable();
@@ -38,39 +36,29 @@ export class CommentsComponent implements OnInit {
         this.stepSubscription = this.appStore.select(selectQuizStep)
             .subscribe(() => this.commentsExpanded.next(false));
 
-        this.itemComments$ = this.appStore.select(selectItemComments);
-        this.itemAnswered$ = this.appStore.select(selectActiveItemAnswered);
-
-        this.comments$ = this.initCommentsObservable();
-    }
-
-    private initCommentsObservable(): Observable<Comment[]> {
-        return this.commentsExpanded$.pipe(
-            switchMap(expanded => !expanded ? of([]) : this.itemAnswered$.pipe(
-                first(),
-                filter((answered) => {
-                    if (!answered) {
-                        this.commentsExpanded.next(false);
-                        this.showAnswerFirstDialog();
-                        return false;
-                    }
-                    return true;
-                }),
-                switchMap(() => this.itemComments$.pipe(filter(x => !!x), first()))
-            ))
+        this.comments$ = combineLatest(
+            this.commentsExpanded$.pipe(filter(x => !!x)),
+            this.appStore.select(selectItemComments)
+        ).pipe(
+            map(([, comments]) => comments)
         );
     }
 
-    private showAnswerFirstDialog() {
-        this.dialogService.alert({
-            title: 'Information',
-            message: 'Please answer first to see comments'
-        });
-    }
-
     showComments() {
-        this.appStore.dispatch(new ActionLoadItemComments());
-        this.commentsExpanded.next(true);
+        this.appStore.select(selectActiveItemAnswered).pipe(
+            take(1)
+        ).subscribe((answered) => {
+            if (!answered) {
+                this.dialogService.alert({
+                    title: 'Information',
+                    message: 'Please answer first to see comments'
+                });
+                return;
+            }
+
+            this.commentsExpanded.next(true);
+            this.appStore.dispatch(new ActionLoadItemComments());
+        });
     }
 
     hideComments() {
