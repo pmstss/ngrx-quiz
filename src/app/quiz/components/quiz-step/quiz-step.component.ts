32304d34778e7daf40a4ad6051ca8b41906c9cbe
Viachaslau Tyshkavets <pmstss@gmail.com>
2019-01-29 11:15:02 +0300

    quiz-step: - quiz-step-choice.component extraction - animation of choices appearing one by one (bounceInLeft) with DELAY_CHOICES_QUEUE - animation of step appearing (fadeIn)

--- src/app/quiz/components/quiz-step/quiz-step.component.ts
+++ src/app/quiz/components/quiz-step/quiz-step.component.ts
@@ -1,35 +1,67 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { Subscription, combineLatest, Observable, of, Subject, BehaviorSubject } from 'rxjs';
-import { map, filter, take, switchMap } from 'rxjs/operators';
+import { trigger, transition, style, animate, keyframes, useAnimation } from '@angular/animations';
+import { Subscription, combineLatest, Observable, of, BehaviorSubject, from } from 'rxjs';
+import { map, filter, take, switchMap, delay, concatMap, tap, distinctUntilChanged } from 'rxjs/operators';
 import { Store } from '@ngrx/store';
+import { bounceInLeft, fadeIn } from 'ng-animate';
 import { Comment } from 'ngrx-quiz-common';
 import {
-    AppState, ActionSubmitAnswer, ActionToggleChoice, ActionLoadItem,
-    QuizState, selectQuizState, QuizItemStatus, selectActiveItemStatus, selectQuizId, selectQuizActiveItemId
+    AppState, QuizState, QuizItemStatus, QuizItemChoiceStatus,
+    ActionSubmitAnswer, ActionToggleChoice, ActionLoadItem,
+    selectQuizState, selectActiveItemStatus, selectQuizId, selectQuizActiveItemId
 } from '../../../store';
 import { AutoUnsubscribe, QuizService } from '../../../core';
+
+const DELAY_CHOICES_QUEUE = 150;
 
 @Component({
     selector: 'app-quiz-step',
     templateUrl: './quiz-step.component.html',
-    styleUrls: ['./quiz-step.component.scss']
+    styleUrls: ['./quiz-step.component.scss'],
+    animations: [
+        trigger('fadeIn', [transition('* => *', useAnimation(fadeIn, {
+            params: { timing: 0.6, delay: 0 }
+        }))]),
+        trigger('bounceInLeft', [transition(':enter', useAnimation(bounceInLeft, {
+            params: { timing: 0.5, delay: 0, a: '-80%' }
+        }))]),
+        // trigger('zoomInLeft', [transition('* => *', useAnimation(zoomInLeft))]),
+        // trigger('flipInX', [transition('* => *', useAnimation(flipInX))]),
+        trigger('flyInAnimation', [
+            transition(':enter', [
+                style({
+                    transform: 'translate3d(-100%, 0, 0)'
+                }),
+                animate('0.5s ease-out', keyframes([
+                    style({
+                        transform: 'translate3d(0, 0, 0)',
+                        offset: 1
+                    })
+                ]))
+            ])
+        ])
+    ]
 })
 export class QuizStepComponent implements OnInit {
     @AutoUnsubscribe private routeSubscription: Subscription;
     quizState$: Observable<QuizState>;
-    itemState$: Observable<QuizItemStatus>;
+    itemStatus$: Observable<QuizItemStatus>;
+    choices$: Observable<QuizItemChoiceStatus[]>;
+
+    flyCounter: number = 0;
 
     private commentsSubject = new BehaviorSubject<Comment[]>([]);
     comments$ = this.commentsSubject.asObservable();
     commentsExpanded = false;
 
     constructor(private route: ActivatedRoute, private appStore: Store<AppState>, private quizService: QuizService) {
-        this.quizState$ = this.appStore.select(selectQuizState);
-        this.itemState$ = this.appStore.select(selectActiveItemStatus).pipe(filter(v => !!v));
     }
 
     ngOnInit() {
+        this.quizState$ = this.appStore.select(selectQuizState);
+        this.itemStatus$ = this.appStore.select(selectActiveItemStatus).pipe(filter(v => !!v));
+
         this.routeSubscription = combineLatest(
             this.appStore.select(selectQuizId),
             this.route.params.pipe(
@@ -40,14 +72,22 @@ export class QuizStepComponent implements OnInit {
         ).subscribe(
             ([quizId, step]) => this.appStore.dispatch(new ActionLoadItem({ step, quizId }))
         );
+
+        this.choices$ = this.itemStatus$.pipe(
+            map(status => status.choicesStatus),
+            distinctUntilChanged(
+                (ch1, ch2) => ch1.length === ch2.length && ch1.every((ch, idx) => ch.id === ch2[idx].id)
+            ),
+            tap(() => this.flyCounter = 0),
+            switchMap((choices: QuizItemChoiceStatus[]) =>
+                from(choices.map((ch, idx) => choices.slice(0, idx + 1)))
+                    .pipe(concatMap(ch => of(ch).pipe(delay(DELAY_CHOICES_QUEUE))))
+            )
+        );
     }
 
     submit() {
         this.appStore.dispatch(new ActionSubmitAnswer());
-    }
-
-    toggleChoice(choiceId: string): void {
-        this.appStore.dispatch(new ActionToggleChoice({ choiceId }));
     }
 
     loadComments() {
@@ -60,12 +100,22 @@ export class QuizStepComponent implements OnInit {
     }
 
     addComment({ text }: {text: string}) {
-        this.itemState$.pipe(
+        this.itemStatus$.pipe(
             take(1),
             switchMap(state => this.quizService.postComment(state.id, text))
         ).subscribe((res) => {
             const comments = [res].concat(this.commentsSubject.value);
             this.commentsSubject.next(comments);
         });
+    }
+
+    animationDone(event: any) {
+        if (event.fromState === 'void') {
+            this.flyCounter++;
+        }
+    }
+
+    trackingFunction(index: number, obj: any) {
+        return obj.id;
     }
 }
