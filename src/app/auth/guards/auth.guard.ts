c0b9be085f8eca0362e619c373c32ac8e1c37edd
Viachaslau Tyshkavets <pmstss@gmail.com>
2019-02-07 13:33:18 +0300

    auth.guard: re-enabling, "Please log in first" message

--- src/app/auth/guards/auth.guard.ts
+++ src/app/auth/guards/auth.guard.ts
@@ -1,19 +1,28 @@
 import { Injectable } from '@angular/core';
 import { CanActivate, Router, UrlTree } from '@angular/router';
 import { Observable, of } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { switchMap, map } from 'rxjs/operators';
 import { Store } from '@ngrx/store';
+import { User } from 'ngrx-quiz-common';
 import { AppState, selectUser } from '../../store';
+import { DialogService } from '../../dialog';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
-    constructor(private router: Router, private appStore: Store<AppState>) {
+    constructor(private router: Router, private appStore: Store<AppState>, private dialogService: DialogService) {
     }
 
     canActivate(): Observable<boolean | UrlTree> {
-        return of(true);
-        /*return this.appStore.select(selectUser).pipe(
-            map(user => user ? true : this.router.parseUrl('/auth/login'))
-        );*/
+        return this.appStore.select(selectUser).pipe(
+            switchMap((user: User) => {
+                if (user) {
+                    return of(true);
+                }
+
+                return this.dialogService.alert('Please log in first').pipe(
+                    map((res: boolean) => res && this.router.parseUrl('/auth/login'))
+                );
+            })
+        );
     }
 }
