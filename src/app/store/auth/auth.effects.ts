bd2131c0df3d83725a59ee380db99e085768446e
Viachaslau Tyshkavets <pmstss@gmail.com>
2019-02-08 14:43:49 +0300

    logged in user greeting toastr via non-dispatchable effect in auth store

--- src/app/store/auth/auth.effects.ts
+++ src/app/store/auth/auth.effects.ts
@@ -1,7 +1,20 @@
 import { Injectable } from '@angular/core';
-import { Actions } from '@ngrx/effects';
+import { tap } from 'rxjs/operators';
+import { Actions, Effect, ofType } from '@ngrx/effects';
+import { AuthActionTypes, ActionLogin } from './auth.actions';
+import { MessageService } from 'src/app/core';
 
 @Injectable()
 export class AuthEffects {
-    constructor(private actions$: Actions) { }
+    constructor(private actions$: Actions, private messageService: MessageService) { }
+
+    @Effect({ dispatch: false })
+    resetQuizSuccess$ = this.actions$.pipe(
+        ofType(AuthActionTypes.LOGIN),
+        tap((action: ActionLogin) => {
+            const userName = action.payload.user.fullName;
+            this.messageService.success(`Hello, ${userName}!`, 'Success Login');
+        })
+    );
+
 }
