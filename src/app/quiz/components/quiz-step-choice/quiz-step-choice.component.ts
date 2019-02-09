f4cf5d1e48ff03437b88134e767a550e657df56c
Viachaslau Tyshkavets <pmstss@gmail.com>
2019-02-09 19:07:53 +0300

    quiz-step-choice.component: choice state (checked/correct/wrong) animations

--- src/app/quiz/components/quiz-step-choice/quiz-step-choice.component.ts
+++ src/app/quiz/components/quiz-step-choice/quiz-step-choice.component.ts
@@ -1,12 +1,21 @@
-import { Component, OnInit, Input, ChangeDetectionStrategy } from '@angular/core';
+import { Component, Input, ChangeDetectionStrategy } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { QuizItemChoiceStatus, AppState, ActionToggleChoice } from '../../../store';
+import { shake, rubberBand, pulse } from 'ng-animate';
+import { trigger, transition, useAnimation } from '@angular/animations';
 
 @Component({
     selector: 'app-quiz-step-choice',
     templateUrl: './quiz-step-choice.component.html',
     styleUrls: ['./quiz-step-choice.component.scss'],
-    changeDetection: ChangeDetectionStrategy.OnPush
+    changeDetection: ChangeDetectionStrategy.OnPush,
+    animations: [
+        trigger('myShake', [
+            transition('* => wrong', useAnimation(shake)),
+            transition('* => correct', useAnimation(rubberBand), { params: { timing: 0.4 } }),
+            transition('none => checked', useAnimation(pulse), { params: { scale: 1.05, timing: 0.3 } })
+        ])
+    ]
 })
 export class QuizStepChoiceComponent {
     @Input() answered: boolean;
@@ -16,5 +25,15 @@ export class QuizStepChoiceComponent {
 
     toggleChoice(choiceId: string): void {
         this.appStore.dispatch(new ActionToggleChoice({ choiceId }));
+    }
+
+    getAnimationState(choice: QuizItemChoiceStatus): string {
+        if (this.answered && choice.checked) {
+            return choice.wrong ? 'wrong' : 'correct';
+        }
+        if (choice.checked) {
+            return 'checked';
+        }
+        return 'none';
     }
 }
