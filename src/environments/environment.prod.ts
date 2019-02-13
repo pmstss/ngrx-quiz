732b1fd98782dc0d84bd59cc2d40a57c88530589
Viachaslau Tyshkavets <pmstss@gmail.com>
2019-02-13 03:52:44 +0300

    https://api.checkme.pro urls

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: true,
-    apiUrl: 'https://ngrx-quiz-api.herokuapp.com',
+    apiUrl: 'https://api.rankme.pro',
     captchaKey: '6LeRE5EUAAAAAO_0L459Kil6tR1ZQEF2kQMRI-Lr',
     oauthGoogleUrl: '/assets/oauth-google.html',
     oauthGithubUrl: '/assets/oauth-github.html'
