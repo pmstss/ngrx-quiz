cdaac5e14edaec834065a6c5a6ce3e89bda8d701
Viachaslau Tyshkavets <pmstss@gmail.com>
2019-02-13 03:00:55 +0300

    temp switch to heroku

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: true,
-    apiUrl: 'http://api.rankme.pro',
+    apiUrl: 'https://ngrx-quiz-api.herokuapp.com',
     captchaKey: '6LeRE5EUAAAAAO_0L459Kil6tR1ZQEF2kQMRI-Lr',
     oauthGoogleUrl: '/assets/oauth-google.html',
     oauthGithubUrl: '/assets/oauth-github.html'
